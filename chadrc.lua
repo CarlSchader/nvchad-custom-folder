6895fa2567be4a39cfad809a847c40103d9353a5
Carl Schader <carlschader@gmail.com>
2024-03-22 13:00:42 -0600

    python highlighting

--- chadrc.lua
+++ chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "one_light",
+  theme = "wombat",
   theme_toggle = { "wombat", "one_light" },
 
   hl_override = highlights.override,
